5f5b61f95aab66d826f7a35066bad8ae450f9430
Kudret Serin <kserin@gmail.com>
2016-03-13 18:42:29 +0200

    add page layout

--- resolutions/App.jsx
+++ resolutions/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-class App extends React.Component{
+export default class App extends React.Component{
 
     render(){
-        <h1> Hello World </h1>
+        return (
+            <h1> Hello World </h1>
+        )
     }
-}
-
-if(Meteor.isClient){
-    Meteor.startup(function(){
-        React.render(<App/>, document.getElementById('render-target'));
-    })
 }
